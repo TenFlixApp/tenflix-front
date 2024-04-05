a735b513a510e0d223fa3ea0397d75694be3e06e
Thomas <thomas.blondel@viacesi.fr>
2024-04-05 14:04:53 +0200

    ajout navbar mobil + maj navbar pc

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,18 @@
     link: string;
     perm: string;
     }
+    // Définition des notifs
+    interface Notif {
+    titre: string;
+    chaine: string;
+    video: string;
+    }
+
+    const notifs = ref<Notif[]>([
+        { titre: "Nouvelle video1", chaine: "chaine1", video: "https://cdn.vuetifyjs.com/images/john.jpg" },
+        { titre: "Nouvelle video2", chaine: "chaine2", video: "Titre de la video" },
+        { titre: "Nouvelle video3", chaine: "chaine3", video: "Titre de la video" }
+    ]);
 
     // Utilisation de 'ref' pour la réactivité. Vous pouvez aussi utiliser 'reactive' pour des objets complexes.
     const boutons = ref<Bouton[]>([
@@ -16,9 +28,7 @@
         { titre: "Dashboard", link: "/dashboard" , perm: "admin" },
         { titre: "Gestion comptes", link: "/gestion-comptes" , perm: "admin" }
     ]);
-
- 
-
+    
     const commandes = ref<Bouton[]>([]);
     const searchShowed = ref<boolean>(false);
     const menunotif = ref<boolean>(false);
@@ -35,50 +45,60 @@
 <template>
     <v-app-bar :elevation="2" rounded>
         <!-- Contenu à gauche -->
-        <img class="mr-3" :src="logoPath" height="40"/>
+            <img class="mr-3" :src="logoPath" height="40"/>
 
-        
-        <!-- Itération sur chaque bouton pour créer un bouton par bouton -->
-            <div v-for="bouton in boutons" :key="bouton.titre">
-                <!-- Correction de la méthode commander appelée avec l'objet bouton -->
-                <v-btn variant="plain" color="bar" @click="commander(bouton)">{{ bouton.titre }}</v-btn>
-            </div>
+            
+            <!-- Itération sur chaque bouton pour créer un bouton par bouton -->
+                <div v-for="bouton in boutons" :key="bouton.titre">
+                    <!-- Correction de la méthode commander appelée avec l'objet bouton -->
+                    <v-btn variant="plain" color="bar" @click="commander(bouton)">{{ bouton.titre }}</v-btn>
+                </div>
         
         <v-spacer></v-spacer>
+
         <!-- Contenu à droite -->
           
-        <v-expand-transition>
-            <v-text-field label="Recherche" v-if="searchShowed" ></v-text-field>
-        </v-expand-transition>      
+            <v-expand-transition>
+                <v-text-field label="Recherche" v-if="searchShowed" ></v-text-field>
+            </v-expand-transition>      
 
-                <v-btn variant="plain" icon="mdi-magnify" @click="searchShowed = !searchShowed" ></v-btn>
+            <v-btn variant="plain" icon="mdi-magnify" @click="searchShowed = !searchShowed" ></v-btn>
+            
+            <!-- menu affichage des notif -->
                 <v-menu v-model="menunotif" :location="bottom" :close-on-content-click="false" >
                     <template v-slot:activator="{ props }">
                         <v-btn v-bind="props" variant="plain" icon="mdi-bell" ></v-btn>
                     </template>
                     <v-card min-width="300">
                         <v-list>
-                        <v-list-item
-                            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-                            subtitle="Founder of Vuetify"
-                            title="John Leider"
-                        >
-                        </v-list-item>
+                            <div v-for="notif in notifs" :key="notif.titre">
+                                <v-list-item
+                                    :prepend-avatar="notif.video"
+                                    :subtitle="notif.chaine"
+                                    :title="notif.titre"
+                                >
+                                </v-list-item>
+                            </div >
+                            <!-- <v-list-item
+                                prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+                                subtitle="chaine"
+                                title="Titre de la video"
+                            >
+                            </v-list-item> -->
                         </v-list>
                     </v-card>
                 </v-menu>
+            <!-- menu paramétre compte  -->
                 <v-menu v-model="menucompte" :location="bottom" :close-on-content-click="false" >
                     <template v-slot:activator="{ props  }">
                         <v-btn v-bind="props" variant="plain" icon="mdi-account" ></v-btn>
                     </template>
                     <v-card min-width="300">
                         <v-list>
-                        <v-list-item
-                            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-                            subtitle="Founder of Vuetify"
-                            title="John Leider"
-                        >
-                        </v-list-item>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-cog">Paramétre</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-account">Compte</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-upload">Mettre en ligne</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-logout">Se déconnecter</v-btn>
                         </v-list>
                     </v-card>
                 </v-menu>
---
+++ src/components/Navbarmobile.vue
@@ -0,0 +1,14 @@
+<!-- <script setup lang="ts">
+    import { ref } from 'vue';
+    // Définition du type pour les Boutons et commandes
+
+</script> -->
+<template>
+    <v-app-bar :elevation="2" rounded>
+        <div class="d-flex justify-space-around w-100">
+            <v-btn variant="plain"  prepend-icon="mdi-home" stacked>acceuil</v-btn>
+            <v-btn variant="plain"  prepend-icon="mdi-layers" stacked>abonnement</v-btn>
+            <v-btn variant="plain"  prepend-icon="mdi-menu" stacked>plus</v-btn>
+        </div>
+    </v-app-bar>
+</template>
