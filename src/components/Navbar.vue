ebe9e3a144a187c22338e964724fef2be68e9780
Thomas <thomas.blondel@viacesi.fr>
2024-04-08 09:57:44 +0200

    navbar mobile

---
+++ src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Bouton {
+titre: string;
+link: string;
+perm: string;
+}
+
+const boutons = ref<Bouton[]>([
+    { titre: "Acceuil", link: "/" , perm: "all" },
+    { titre: "Recherche", link: "/recherche" , perm: "all" },
+    { titre: "Playlist", link: "/playlist" , perm: "all" },
+    { titre: "Abonnements", link: "/abonnements" , perm: "all" },
+    { titre: "Dashboard", link: "/dashboard" , perm: "admin" },
+    { titre: "Gestion comptes", link: "/gestion-comptes" , perm: "admin" }
+]);
+
+const searchShowed = ref<boolean>(false);
+const menunotif = ref<boolean>(false);
+const menucompte = ref<boolean>(false);
+const logoPath = ref(' /logo.png');
+
+</script>
+
+<template>
+    <v-app-bar :elevation="2" rounded>
+        <img class="mr-3" :src="logoPath" height="40"/>
+
+            <div v-for="bouton in boutons" :key="bouton.titre">
+                <v-btn variant="plain" color="bar" >{{ bouton.titre }}</v-btn>
+            </div>
+        
+        <v-spacer></v-spacer>
+          
+        <v-expand-transition>
+            <v-text-field hide-details label="Recherche" v-if="searchShowed" ></v-text-field>
+        </v-expand-transition>      
+
+                <v-btn variant="plain" icon="mdi-magnify" @click="searchShowed = !searchShowed" ></v-btn>
+                <v-menu v-model="menunotif" :close-on-content-click="false" >
+                    <template v-slot:activator="{ props }">
+                        <v-btn v-bind="props" variant="plain" icon="mdi-bell" ></v-btn>
+                    </template>
+                    <v-card min-width="300">
+                        <v-list>
+                        <v-list-item
+                            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+                            subtitle="Founder of Vuetify"
+                            title="John Leider"
+                        >
+                        </v-list-item>
+                        </v-list>
+                    </v-card>
+                </v-menu>
+                <v-menu v-model="menucompte" :close-on-content-click="false" >
+                    <template v-slot:activator="{ props  }">
+                        <v-btn v-bind="props" variant="plain" icon="mdi-account" ></v-btn>
+                    </template>
+                    <v-card min-width="300">
+                        <v-list>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-cog">Paramétre</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-account">Compte</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-upload">Mettre en ligne</v-btn><br>
+                            <v-btn variant="plain" color="bar" prepend-icon="mdi-logout">Se déconnecter</v-btn>
+                        </v-list>
+                    </v-card>
+                </v-menu>
+    </v-app-bar>
+</template>
+  
--- src/components/Navbarmobile.vue
+++ src/components/Navbarmobile.vue
@@ -4,11 +4,20 @@
 
 </script> -->
 <template>
-    <v-app-bar :elevation="2" rounded>
+    <div class="d-flex align-end div-en-bas ">
         <div class="d-flex justify-space-around w-100">
             <v-btn variant="plain"  prepend-icon="mdi-home" stacked>acceuil</v-btn>
             <v-btn variant="plain"  prepend-icon="mdi-layers" stacked>abonnement</v-btn>
             <v-btn variant="plain"  prepend-icon="mdi-menu" stacked>plus</v-btn>
         </div>
-    </v-app-bar>
+    </div>
 </template>
+<style>
+    .div-en-bas {
+    position: fixed; /* Fixe la div par rapport à la fenêtre du navigateur */
+    bottom: 0; /* Aligner la div au bas de la fenêtre */
+    width: 100%; /* Fait en sorte que la div s'étende sur toute la largeur */
+    text-align: center; /* Centrer le texte à l'intérieur de la div */
+    padding: 20px 0; /* Ajouter un peu d'espace au-dessus et en dessous du texte */
+}
+</style>
